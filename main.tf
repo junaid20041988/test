f5b905f266b05137a065c92f662433afa1239cef
Your Name <you@example.com>
2025-05-28 13:50:44 +0530

    1st com

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "tf-test-bucket-1191"
+
+  tags = {
+    Name        = "tf bucket"
+    Env		    = "Dev"
+  }
+}
